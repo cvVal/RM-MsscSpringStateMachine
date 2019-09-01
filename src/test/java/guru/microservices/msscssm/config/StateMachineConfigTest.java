a1eb668771f6e05547c25197976592f79450a5d4
cvVal <cvvaldez_00@hotmail.com>
2019-09-01 12:59:32 -0500

    Add state changed configuration

--- src/test/java/guru/microservices/msscssm/config/StateMachineConfigTest.java
+++ src/test/java/guru/microservices/msscssm/config/StateMachineConfigTest.java
@@ -32,5 +32,9 @@ class StateMachineConfigTest {
         sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
 
         System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+
+        System.out.println(sm.getState().toString());
     }
 }
